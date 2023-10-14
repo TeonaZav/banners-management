a8e7edff164bf37219439ff52a4384c30900fb6c
Teona Zavrashvili <101217572+TeonaZav@users.noreply.github.com>
2023-10-14 09:49:49 +0400

    add environment variables

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  domain: 'https://development.api.optio.ai/api/v2/',
+  accessToken: `Bearer ${'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImludGVybnNoaXBAb3B0aW8uYWkiLCJzdWIiOiJpbnRlcm5zaGlwIiwiaW50ZXJuc2hpcElkIjoidGVvbmEuemF2cmFzaHZpbGlAZ21haWwuY29tIiwiaWF0IjoxNjk2NjUwODIyLCJleHAiOjE2OTc1MTQ4MjJ9.vcnANdO6SMv2FSai9UwYyOPfUY97XRifAJz4v6qFKgNQrz60uhSiJ8RRphaxWXhXI3S61XKIfa2IXiXk5fA4xQ'}`,
+};
